1569da74c7d43c7b1e0bfd6f2770f1a169f0a6ef
chariot <107884346+jpeglp@users.noreply.github.com>
2023-10-17 21:49:42 -0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -65,7 +65,7 @@ class States extends Colors {
 }
 
 class Constants {
-    static IPA_FETCH_LINK = "https://cdn.discordapp.com/attachments/755496627976011828/1161476659820625971/Discord_200.0_50625.ipa";
+    static IPA_FETCH_LINK = "https://cdn.discordapp.com/attachments/755496627976011828/1164062070648614943/Discord_201.0_50952.ipa";
 }
 
 export { Shell, Colors, Divider, States, Constants };
