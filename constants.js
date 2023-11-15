0dee2510452c85b357840d04c1a88569f90d902e
chariot <107884346+jpeglp@users.noreply.github.com>
2023-11-15 10:50:01 -0800

    Update constants.js

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_204.0_51761.ipa";
+Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_206.0_51992.ipa";
 export { Shell, Colors, Divider, States, Constants };
